8a61b7403aadf170e745d9d9eb110f2c159101d1
jwalla7 <j7wallace7@outlook.com>
2022-07-09 16:53:15 -0500

    Jul 9 0453

---
+++ SQL/WorldDatabase.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+all_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="MACjrw20148!"
+)
+
+print(all_db)
+
+# Check if Database Exisits
+input_cursor = all_db.cursor()
+input_cursor.execute("SHOW DATABASES")
+
+for db in input_cursor:
+    print(db)
+
+# World Database
+world_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="MACjrw20148!",
+    database="world"
+)
+
+
+
